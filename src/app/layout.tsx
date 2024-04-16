6f194364d9fa5fbba6873738eb38936437766cc6
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-16 21:12:23 +0500

    add: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
+import { Header } from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
      <body className={inter.className}>
         <Providers>
+        <Header />
           <div className="container mx-auto">{children}</div>
         </Providers>
       </body>
